fa2c4d76ef55e07ac5c5c60fbae1b655603d72d5
theFlyHalfBoy <flyhalfboy@gmail.com>
2019-01-15 15:02:37 +0000

    Setting up routing

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
+
+// Express setup
 const express = require('express');
 const app = express();
 const port = 1337;
 
+// Defining static directory
 app.use(express.static('static'));
 
+// Defining views
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.get('/', (req, res) => res.render('main_desktop_view'));
+// Dealing with GET requests on all subdomains
+app.get('/', (req, res) => res.render('home_view'));
+app.get('/desktop', (req, res) => res.render('init_desktop_view'));
+app.get('/mobile', (req, res) => res.render('init_mobile_view'));
 
+// Listening...
 app.listen(port, () => console.log('Example app listening on port ' + port));
-
