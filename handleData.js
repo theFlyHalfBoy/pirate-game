cc20d9530c2c3e3befbaf218e963c9094a9e1de2
theFlyHalfBoy <flyhalfboy@gmail.com>
2019-02-11 15:05:50 +0000

    Grid Validation complete

--- handleData.js
+++ handleData.js
@@ -20,10 +20,75 @@ function noDuplicateNames(array, name, num) {
         // the last two characters if they are present (if num != 0)
         return noDuplicateNames(array, (num == 0) ? name + '_' + (num + 1).toString(10) : name.slice(0, -2) + '_' + (num + 1).toString(10), num + 1);
     };
-}
+};
 
-// Defining what the module will export using Node's preferred
-// notation which allows my import statements to be consistent
+// Ensures the grid contains a) only valid cell type names, and
+//                           b) the correct number of each cell type
+function validateGrid(grid, validInputs, validInputTotals) {
+    
+    // Initialises an array the same length as
+    let cellTypeCounts = new Array(validInputs.length).fill(0);
+
+    // Iterates through each entry in the one-dimensional grid
+    for (let cell in grid) {
+
+        // Finding the index of the cell type in the list of valid inputs,
+        // which in turn specifies the type of cell it is. Returns -1 if
+        // the data is invalid
+        let cellTypeIndex = validInputs.findIndex((element) => {return element == grid[cell]});
+        
+        // Checks if the inputted data was valid, returns false if not
+        if (index == -1) {
+            return false;
+        } else {
+
+            // Increments the respective count of the cell type
+            cellTypeCounts[cellTypeIndex] ++;
+
+            // Checks that the recently incremented cell type count is
+            // less than or equal to the expected value, returns false if not
+            if (cellTypeCounts[cellTypeIndex] > validInputTotals[cellTypeIndex]) {
+                return false;
+            };
+        };
+    };
+
+    // Returns true if the grid passes both tests on every cell
+    return true;
+};
+
+// Function to convert the player's form input on the grid page into a 2D array
+function make2DArray(data, rows, cols) {
+
+    // Defining vars
+    let grid = [];
+    let r = 0;
+    let c = 0;
+
+    // Iterating through rows and cols to initialise a blank
+    // 2D array of the correct size 
+    for (let i = 0; i < rows; i++) {
+        grid.push([]);
+        for (let j = 0; j < cols; j++) {
+            grid[i].push("");
+        };
+    };
+
+    // Iterating through the data and inserting it into the blank grid
+    for (let cell in data) {
+
+        // Using integer and remainder division to insert the data correctly
+        grid[parseInt(r/rows)][c%cols] = data[cell];
+        r++;
+        c++;
+    };
+
+    // Returns the 2D-ified grid
+    return grid;
+};
+
+// Defining what the module will export using Node's preferred notation,
+// which allows my import statements to be consistent
 module.exports = {
 
 	initialiseGameData: function(id) {
@@ -60,7 +125,7 @@ module.exports = {
 			gameDataJSON,
 			'utf8',
 			() => console.log('Local JSON database initialised')
-			);
+        );
 	},
 
     initialiseNewPlayer: function(formData) {
@@ -114,7 +179,44 @@ module.exports = {
         return formData.game_id + newName;
     },
 
-    initialisePlayerGrid: function(formData) {
-        console.log(formData);
+    initialisePlayerGrid: function(playerGrid, playerID, validInputs, validInputTotals) {
+        
+        // Removes the unnecessary 'request type' parameter from the POST request
+        delete playerGrid.req_type;
+
+        // Ensures that the grid is valid (contains only valid cell types,
+        // and the correct amount of each)
+        // if (!validateGrid(playerGrid, validInputs, validInputTotals)) {
+        //     return "invalid";
+        // };
+
+        // Converts the player's form data to a 2D array
+        let grid2D = make2DArray(playerGrid, 7, 7);
+
+        // Separates the game ID from the nickname in the cookie
+        let gameID = playerID.slice(0, 4);
+        let playerName = playerID.slice(4);
+
+        // Reading the JSON from file
+        var gameData = JSON.parse(fs.readFileSync('gameData' + gameID + '.json'));
+
+        // Iterates through each of the players in the database
+        for (let playerI of gameData.players) {
+
+            // Inserts the inputted grid into the correct player entry in the database
+            if (playerI.nickname == playerName) {
+                playerI.grid = grid2D;
+                break;
+            };
+        };
+
+        // Converting the object back to JSON
+        var gameDataJSON = JSON.stringify(gameData);
+
+        // Writing the JSON string back to the JSON file
+        fs.writeFileSync('gameData' + gameID + '.json', gameDataJSON);
+
+        // Returns the cleaned up grid, to be saved as a cookie on the client
+        return playerGrid;
     } 
 }
--- server.js
+++ server.js
@@ -19,12 +19,14 @@ app.use(bodyParser.json());
 
 app.use(cookieParser());
 
-// Defining static directory
+// Defining static files directory
 app.use(express.static('static'));
 
 // Defining views with Pug
 app.set('view engine', 'pug');
 app.set('views', './views');
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 // Dealing with GET requests on all subdomains
 app.get('/', (req, res) => res.render('home_view'));
@@ -40,6 +42,8 @@ app.get('/desktop', (req, res) => {
 
 app.get('/mobile', (req, res) => res.render('init_mobile_view'));
 
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
 // Dealing with POST requests on specific subdomains
 app.post('/mobile', (req, res) => {
 
@@ -50,17 +54,35 @@ app.post('/mobile', (req, res) => {
     // This denotes a request from the player login page
     if (reqType == 'login_page') {
 
-        // Initialises a new player using their form inputs, and sets a cookie on the
-        res.cookie('id', handleData.initialiseNewPlayer(req.body)).render('input_mobile_view');
+        // Initialises a new player using their form inputs, sets a cookie on the browser
+        // that saves the game ID and unique nickname in one unique string, and renders 
+        // the next mobile page
+        res.cookie('player_id', handleData.initialiseNewPlayer(req.body)).render('input_mobile_view');
 
+    // This denotes a request from the player grid input sreen
     } else if (reqType == 'player_grid') {
 
-        // Initialises
-        let grid = handleData.initialisePlayerGrid(req.body);
+        // Initialises the player grid in the JSON database, passing the accepted values
+        // and their respective expected counts to the function
+        let grid = handleData.initialisePlayerGrid(req.body, req.cookies.player_id,
+        ["200", "1000", "3000", "5000", "shield", "mirror", "knife", "choose", "double", "bomb", "swap", "skull", "gift", "rob", "bank"],
+        [ 25,    10,     2,      1,      1,        1,        1,       1,        1,        1,      1,      1,       1,      1,     1    ]);
 
-        res.cookie('grid', grid).render('game_mobile_view');
+        // Checks the result of intitialisePlayerGrid, and re-renders the grid page if required
+        if (grid == "invalid") {
+            res.render('input_mobile_view');
+        } else {
+            // Otherwise, sets a cookie with the player's personal grid, and renders the next page
+            res.cookie('player_grid', grid).render('game_mobile_view', { grid: grid });
+        };
+
+    // Denotes an unrecognised request: logs an error but does nothing otherwise
+    } else {
+        console.log("Request not recognised");
     }
 });
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 // Listening...
 app.listen(port, () => console.log('Pirate Game listening on port ' + port));
