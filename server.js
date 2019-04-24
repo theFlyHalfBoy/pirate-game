7bad6a9e4c2e41fc83985e727dc204ace262a5c7
theFlyHalfBoy <flyhalfboy@gmail.com>
2019-04-24 17:23:58 +0100

    Fixed port for Heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 1337;
+const port = process.env.PORT || 1337;
 
 app.use(bodyParser.urlencoded({
     extended: true,
